e667c90d29116c1fe7d6b2123045ad3755b5239a
dayeonii <daabbong@gmail.com>
2024-11-15 22:38:12 +0900

    댓글 등록 시 사용자 아이디 등록, 댓글 등록/삭제 시 카운트 변경

--- recipe/recipe/src/main/java/com/example/recipe/controller/CommentController.java
+++ recipe/recipe/src/main/java/com/example/recipe/controller/CommentController.java
@@ -2,7 +2,10 @@ package com.example.recipe.controller;
 
 import com.example.recipe.dto.CommentRequest;
 import com.example.recipe.entity.Comment;
+import com.example.recipe.entity.Member;
 import com.example.recipe.service.CommentService;
+import com.example.recipe.service.GradeService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +17,28 @@ import java.util.List;
 public class CommentController {
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private GradeService gradeService;
 
     // 댓글 작성
-    @PostMapping
-    public Comment addComment(@RequestBody CommentRequest commentRequest) {
-        return commentService.addComment(
+    @PostMapping("/create")
+    public Comment addComment(@RequestBody CommentRequest commentRequest, HttpSession session) {
+        // 댓글 작성한 사용자 저장
+        Member currentUser = (Member) session.getAttribute("currentUser");
+        if (currentUser == null) {
+            throw new RuntimeException("로그인 상태가 아닙니다.");
+        }
+
+        Comment createdComment = commentService.addComment(
                 commentRequest.getRecipeId(),
                 commentRequest.getUserId(),
                 commentRequest.getContent()
         );
+
+        gradeService.increaseCommentCount(currentUser.getUserId());
+        gradeService.updateMemberGreade(currentUser.getUserId());
+
+        return createdComment;
     }
 
     // 댓글 수정
@@ -51,6 +67,8 @@ public class CommentController {
     ) {
         try {
             commentService.deleteComment(commentId, userId);
+            gradeService.decreaseCommentCount(userId);
+            gradeService.updateMemberGreade(userId);
             return ResponseEntity.ok("댓글이 삭제되었습니다.");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body("댓글을 삭제할 수 없습니다.");
