cf3db9e503b64545db7543374866f908fd16e5f0
zi-eunn <jieunkim1117@gmail.com>
2024-11-16 20:37:56 +0900

    html 수정, 검색 기능 수정 후 테스트, 수정한 부분 주석으로 표시

--- recipe/recipe/src/main/java/com/example/recipe/controller/SearchController.java
+++ recipe/recipe/src/main/java/com/example/recipe/controller/SearchController.java
@@ -1,8 +1,49 @@
+//package com.example.recipe.controller;
+//
+//import com.example.recipe.entity.Recipe;
+//import com.example.recipe.service.RecipeSearchService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import java.util.List;
+//
+//@Controller
+//public class SearchController {
+//
+//    @Autowired
+//    private RecipeSearchService recipeSearchService;
+//
+//    @GetMapping("/search")
+//    public String searchRecipes(@RequestParam("searchType") String searchType,
+//                                @RequestParam("keyword") String keyword, Model model) {
+//        List<Recipe> recipes;
+//
+//        if ("title".equals(searchType)) {
+//            // 제목으로 검색
+//            recipes = recipeSearchService.searchTitle(keyword);
+//        } else {
+//            // 재료로 검색
+//            recipes = recipeSearchService.searchIngredients(keyword);
+//        }
+//
+//        model.addAttribute("recipes", recipes);
+//        model.addAttribute("keyword", keyword);
+//        return "search result";
+//    }
+//}
+
+//전체 코드 수정, 위 코드는 혹시나해서 남겨둠
 package com.example.recipe.controller;
 
 import com.example.recipe.entity.Recipe;
 import com.example.recipe.service.RecipeSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,19 +59,29 @@ public class SearchController {
 
     @GetMapping("/search")
     public String searchRecipes(@RequestParam("searchType") String searchType,
-                                @RequestParam("keyword") String keyword, Model model) {
-        List<Recipe> recipes;
+                                @RequestParam("keyword") String keyword,
+                                @RequestParam(value = "page", defaultValue = "1") int page,
+                                @RequestParam(value = "size", defaultValue = "5") int size,
+                                Model model) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<Recipe> recipePage;
 
         if ("title".equals(searchType)) {
             // 제목으로 검색
-            recipes = recipeSearchService.searchTitle(keyword);
-        } else {
+            recipePage = recipeSearchService.searchTitle(keyword, pageable);
+        } else if ("ingredient".equals(searchType)){
             // 재료로 검색
-            recipes = recipeSearchService.searchIngredients(keyword);
+            recipePage = recipeSearchService.searchIngredients(keyword, pageable);
+        } else {
+            recipePage = Page.empty(pageable);
         }
 
-        model.addAttribute("recipes", recipes);
+        model.addAttribute("recipes", recipePage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", recipePage.getTotalPages());
         model.addAttribute("keyword", keyword);
-        return "search result";
+        model.addAttribute("searchType", searchType);
+
+        return "search";
     }
 }
--- recipe/recipe/src/main/java/com/example/recipe/service/RecipeSearchService.java
+++ recipe/recipe/src/main/java/com/example/recipe/service/RecipeSearchService.java
@@ -1,8 +1,53 @@
-package com.example.recipe.service;
+//package com.example.recipe.service;
+//
+//
+//import com.example.recipe.entity.Recipe;
+//import com.example.recipe.repository.RecipeRepository;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Component
+//public class RecipeSearchService {
+//
+//    private final RecipeRepository recipeRepository;
+//
+//    public RecipeSearchService(RecipeRepository recipeRepository) {
+//        this.recipeRepository = recipeRepository;
+//    }
+//
+//    // 레시피 가져와서 보여주기
+//    public Recipe findRecipeById(Long recipeId) {
+//        return recipeRepository.findById(recipeId).orElse(null);
+//    }
+//
+//    // 제목으로 레시피 검색
+//    public List<Recipe> searchTitle(String keyword) {
+//        List<Recipe> recipes = recipeRepository.findByTitleContaining(keyword);
+//        if (recipes.isEmpty()) {
+//            System.out.println("검색 결과가 없습니다.");
+//        }
+//        return recipes;
+//    }
+//
+//    // 재료로 레시피 검색
+//    public List<Recipe> searchIngredients(String keyword) {
+//        List<Recipe> recipes = recipeRepository.findByIngredientName(keyword);
+//        if (recipes.isEmpty()) {
+//            System.out.println("검색 결과가 없습니다.");
+//        }
+//        return recipes;
+//    }
+//}
 
+//전체 코드 수정, 위 코드는 혹시나해서 남겨둠
+
+package com.example.recipe.service;
 
 import com.example.recipe.entity.Recipe;
 import com.example.recipe.repository.RecipeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -22,20 +67,12 @@ public class RecipeSearchService {
     }
 
     // 제목으로 레시피 검색
-    public List<Recipe> searchTitle(String keyword) {
-        List<Recipe> recipes = recipeRepository.findByTitleContaining(keyword);
-        if (recipes.isEmpty()) {
-            System.out.println("검색 결과가 없습니다.");
-        }
-        return recipes;
+    public Page<Recipe> searchTitle(String keyword, Pageable pageable) {
+        return recipeRepository.findByTitleContaining(keyword, pageable);
     }
 
     // 재료로 레시피 검색
-    public List<Recipe> searchIngredients(String keyword) {
-        List<Recipe> recipes = recipeRepository.findByIngredientName(keyword);
-        if (recipes.isEmpty()) {
-            System.out.println("검색 결과가 없습니다.");
-        }
-        return recipes;
+    public Page<Recipe> searchIngredients(String keyword, Pageable pageable) {
+        return recipeRepository.findByIngredientContaining(keyword, pageable);
     }
 }
